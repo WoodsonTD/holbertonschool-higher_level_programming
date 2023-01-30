4b33c0ac5e0515d81bdee3600ff5187aa632388c
WoodsonTD <taylodwoodson@gmail.com>
2023-01-30 14:02:59 -0800

    extra underscore?

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """placeholder"""
-BaseGeometry = _import_('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """placeholder"""
-    def _init_(self, width, height):
-        self._width = width
-        self._height = height
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)

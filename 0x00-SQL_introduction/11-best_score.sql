352dbc541bd08a581da656249bc2dec6507e595f
WoodsonTD <taylordwoodson@gmail.com>
2023-02-13 19:21:27 -0600

    made one

--- 0x00-SQL_introduction/11-best_score.sql
+++ 0x00-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
 -- List all records with score >= 10 second_table
-SELECT score >= 10, name FROM second_table
-    ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

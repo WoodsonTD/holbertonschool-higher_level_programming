a2f78035994057185927c2ed5086c01a5dd8a1a5
WoodsonTD <taylordwoodson@gmail.com>
2023-02-13 19:05:22 -0600

    add

---
+++ 0x00-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List all records with score >= 10 second_table
+SELECT score >= 10, name FROM second_table
+    ORDER BY score DESC;
---
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Delete Database hbtn_0c_0
+DROP Database IF EXISTS hbtn_0c_0

6d3cb8fa9160ff5e1383af916f75c1407a0eadf1
WoodsonTD <taylordwoodson@gmail.com>
2023-02-13 19:13:12 -0600

    moved around

--- 0x00-SQL_introduction/4-first_table.sql
+++ 0x00-SQL_introduction/4-first_table.sql
@@ -1,4 +1,2 @@
 -- Create first_table in current database
-CREATE TABLE IF NOT EXISTS first_table
-    id INT,
-    name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));

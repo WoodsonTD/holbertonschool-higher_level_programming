db4396b741d31150bd5ce9ac3a75902eaa60ae74
WoodsonTD <taylordwoodson@gmail.com>
2023-02-28 13:10:38 -0600

    final push

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    
+    # Connecting to the database
+    db = MySQLdb.connect(
+        host="locolhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    # creating a cursor object
+    cursor = db.cursor()
+    
+    # Executing the query
+    results = cursor.fetchall()
+    
+    # Displaying the results
+    for state in results:
+        if state[0] <= 5:
+            print(state)
+            
+    # Closing the connection to the database
+    db.close()
---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists state name matching argv[4] from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    state_name = sys.argv[4]
+
+    # Connecting to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Creating a cursor object
+    cursor = db.cursor()
+
+    # Executing the query
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+        ORDER BY id ASC".format(state_name))
+
+    # Retrieving the results
+    results = cursor.fetchall()
+
+    # Displaying the results
+    for state in results:
+        print(state)
+
+    # Closing the connection to the database
+    db.close()

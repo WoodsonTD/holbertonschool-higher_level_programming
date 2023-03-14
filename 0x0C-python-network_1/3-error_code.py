7751a02d5a945bf59befca95c2b18337f17d73e2
WoodsonTD <taylordwoodson@gmail.com>
2023-03-14 13:11:48 -0500

    add

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -4,15 +4,16 @@ Python script that takes in a URL,
 sends a request to the URL and displays
 the body of the response (decoded in utf-8)
 """
-from urllib import request, error
-import sys
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
 
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    req = request.Request(url)
+if __name__ == '__main__':
     try:
-        with request.urlopen(req) as response:
-            print(response.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        with urlopen(argv[1]) as response:
+            body = response.read()
+            decodedbody = body.decode("utf-8")
+            print(decodedbody)
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
